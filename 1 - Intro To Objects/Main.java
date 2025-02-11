20fa48048b818d88daf7f2c1db849a3914f830a8
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-02-11 11:28:22 -0500

    Create Main.java

---
+++ 1 - Intro To Objects/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        JarOfMoney j1 = new JarOfMoney(5);
+        j1.deposit(3);
+        System.out.println(j1.getAmnt);
+    }
+}
